3e1c690717452ab526e27dd29e2e500211b3200e
Simon Cropp <simon.cropp@gmail.com>
2018-05-24 17:06:15 +1000

    doco

--- NServiceBus.AuditFilter/Attributes/IncludeInAuditAttribute.cs
+++ NServiceBus.AuditFilter/Attributes/IncludeInAuditAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace NServiceBus.AuditFilter
 {
+    /// <summary>
+    /// Used to include a message in being sent to the Audit queue.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class IncludeInAuditAttribute : Attribute
     {
