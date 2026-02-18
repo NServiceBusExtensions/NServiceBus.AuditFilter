63cb868486dd09f9d8008bc2eca2b7676d4e8d47
Simon Cropp <simon.cropp@gmail.com>
2026-02-19 06:20:11 +1100

    refs and cleanup

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,69 +1,71 @@
-﻿public class Tests
+﻿using System.Threading.Tasks;
+
+public class Tests
 {
-    [Fact]
+    [Test]
     public async Task Skip_with_attribute_and_default_to_include()
     {
         var message = new MessageWithExcludeFromAudit();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.IncludeInAudit));
-        Assert.Empty(result);
+        await Assert.That(result).IsEmpty();
     }
 
-    [Fact]
+    [Test]
     public async Task Skip_with_attribute_and_default_to_exclude()
     {
         var message = new MessageWithExcludeFromAudit();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.ExcludeFromAudit));
-        Assert.Empty(result);
+        await Assert.That(result).IsEmpty();
     }
 
-    [Fact]
+    [Test]
     public async Task Audit_with_attribute_and_default_to_include()
     {
         var message = new MessageWithIncludeInAudit();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.IncludeInAudit));
-        Assert.Single(result);
+        await Assert.That(result).HasSingleItem();
     }
 
-    [Fact]
+    [Test]
     public async Task Audit_with_attribute_and_default_to_exclude()
     {
         var message = new MessageWithIncludeInAudit();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.ExcludeFromAudit));
-        Assert.Single(result);
+        await Assert.That(result).HasSingleItem();
     }
 
-    [Fact]
+    [Test]
     public async Task Simple_message_and_default_to_include()
     {
         var message = new SimpleMessage();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.IncludeInAudit));
-        Assert.Single(result);
+        await Assert.That(result).HasSingleItem();
     }
 
-    [Fact]
+    [Test]
     public async Task Simple_message_and_default_to_exclude()
     {
         var message = new SimpleMessage();
         var result = await Send(message, _ => _.FilterAuditQueue(FilterResult.ExcludeFromAudit));
-        Assert.Empty(result);
+        await Assert.That(result).IsEmpty();
     }
 
-    [Fact]
+    [Test]
     public async Task Simple_message_and_delegate_to_include()
     {
         var message = new SimpleMessage();
         var result = await Send(message, _ => _.FilterAuditQueue(
             (_, _) => FilterResult.IncludeInAudit));
-        Assert.Single(result);
+        await Assert.That(result).HasSingleItem();
     }
 
-    [Fact]
+    [Test]
     public async Task Simple_message_and_delegate_to_exclude()
     {
         var message = new SimpleMessage();
         var result = await Send(message, _ => _.FilterAuditQueue(
             (_, _) => FilterResult.ExcludeFromAudit));
-        Assert.Empty(result);
+        await Assert.That(result).IsEmpty();
     }
 
     static async Task<List<AuditedMessageData>> Send(
